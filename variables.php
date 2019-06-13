2379504a26e6679e6da74eb7c39660d00cc14df9
Mourad Essid <rached.bkhalifa@gmail.com>
2019-06-13 17:33:12 +0100

    delete gitignore

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php 
+class vars { 
+//chemin 
+public $root_dir = "condisoft"; 
+public $ad_dir = "adm"; 
+//base 
+public $driver = "pdo_mysql"; 
+public $db_host = "127.0.0.1"; 
+public $db_name = "condisoft"; 
+public $db_user = "root"; 
+public $db_pass = ""; 
+public $driverOptions = array('1002'=> "SET NAMES'UTF8' COLLATE 'utf8_general_ci'"); 
+public $charset = 'UTF8'; 
+} 
+?> 
