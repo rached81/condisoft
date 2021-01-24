c27a617d6794a878a028940130286155cf1069ea
Rached Ben Khalifa <rached.bkhalifa@gmail.com>
2021-01-24 16:35:39 +0100

    modif varibles.php, img uploaded, tpl/etatgensys/lib, lib/js/..., lib/css/.., core/library/vendor/symfony/..

--- variables.php
+++ variables.php
@@ -6,9 +6,9 @@ public $ad_dir = "adm";
 //base 
 public $driver = "pdo_mysql"; 
 public $db_host = "127.0.0.1"; 
-public $db_name = "condisoft"; 
-public $db_user = "root"; 
-public $db_pass = ""; 
+public $db_name = "coldex_condisoft"; 
+public $db_user = "coldex"; 
+public $db_pass = "kNfv*666"; 
 public $driverOptions = array('1002'=> "SET NAMES'UTF8' COLLATE 'utf8_general_ci'"); 
 public $charset = 'UTF8'; 
 } 
